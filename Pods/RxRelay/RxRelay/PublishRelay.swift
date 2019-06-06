4a61d89909796b35d30923a27c0e5b62f753826a
Jesse Gatt <jesse.gatt@organizer.com>
2019-06-06 11:09:03 -0700

    resolving build issues with cocoa pods

--- Pods/RxRelay/RxRelay/PublishRelay.swift
+++ Pods/RxRelay/RxRelay/PublishRelay.swift
@@ -1,6 +1,6 @@
 //
 //  PublishRelay.swift
-//  RxCocoa
+//  RxRelay
 //
 //  Created by Krunoslav Zaher on 3/28/15.
 //  Copyright © 2017 Krunoslav Zaher. All rights reserved.
@@ -12,27 +12,25 @@ import RxSwift
 ///
 /// Unlike `PublishSubject` it can't terminate with error or completed.
 public final class PublishRelay<Element>: ObservableType {
-    public typealias E = Element
-
     private let _subject: PublishSubject<Element>
     
     // Accepts `event` and emits it to subscribers
     public func accept(_ event: Element) {
-        _subject.onNext(event)
+        self._subject.onNext(event)
     }
     
     /// Initializes with internal empty subject.
     public init() {
-        _subject = PublishSubject()
+        self._subject = PublishSubject()
     }
 
     /// Subscribes observer
-    public func subscribe<O: ObserverType>(_ observer: O) -> Disposable where O.E == E {
-        return _subject.subscribe(observer)
+    public func subscribe<Observer: ObserverType>(_ observer: Observer) -> Disposable where Observer.Element == Element {
+        return self._subject.subscribe(observer)
     }
     
     /// - returns: Canonical interface for push style sequence
     public func asObservable() -> Observable<Element> {
-        return _subject.asObservable()
+        return self._subject.asObservable()
     }
 }
