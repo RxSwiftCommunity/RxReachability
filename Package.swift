53446b00335ec8959cfdb6b50a306ea08d78f545
brennoumobi <brenno@umobi.com.br>
2020-06-10 15:29:24 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ashleymills/Reachability.swift", .upToNextMajor(from: "5.0.0"))
+        .package(
+            name: "Reachability",
+            url: "https://github.com/ashleymills/Reachability.swift",
+            .upToNextMajor(from: "5.0.0")
+        )
     ],
     targets: [
         .target(
