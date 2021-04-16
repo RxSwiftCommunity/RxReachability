668019a898a65ccbd5657dc6c3e98db5fb35df54
Joseph Mattiello <mail@joemattiello.com>
2021-04-15 23:06:09 -0400

    Swiftlint rules update

--- RxReachability-Example/RxReachability/AppDelegate.swift
+++ RxReachability-Example/RxReachability/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // swiftlint:disable line_length
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
